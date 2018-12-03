2fd1484bb855b7db46c5e8c13cc6b271d2752111
Jan <mail@jezekjan.com>
2018-12-03 08:28:47 +0100

    fixed {}, added schema

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -29,11 +29,11 @@ RSpec.describe UsersController, type: :controller do
   # User. As you add validations to User, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+   {name: "MyName", email: "myEmail@email.com", birthday: 18.years.ago}
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    {name: "MyName", email: "myEmail@email.com", birthday: 12.years.ago}
   }
 
   # This should return the minimal set of values that should be in the session
